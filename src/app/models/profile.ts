7360bebc3c3d6445fc0c47d8e4abad0c0a0a4e18
Pedro Bernardes <pber22620@gmail.com>
2024-05-28 12:59:30 -0300

    subindo arquivos no github
    Subindo arquivos do projeto

---
+++ src/app/models/profile.ts
@@ -0,0 +1,6 @@
+export class Profile {
+    name: string = 'Ratos da Uninove';
+    whatsAppUrl: string = 'https://api.whatsapp.com/send?phone=11994648415&text=Projeto%20Blue%20Organizer%20Seja%20bem%20vindo!';
+    linkedInUrl: string = 'https://github.com/ratosdaunilixo';
+    email: string = 'projetoblueorganizer@8962gmail.com'
+}
